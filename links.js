6fb21f437c467982d21bdd5605088b84b7002046
KASI-pro <78564295+KASI-pro@users.noreply.github.com>
2025-06-24 11:19:52 +0900

    Add files via upload

---
+++ links.js
@@ -0,0 +1,11 @@
+const voiceLinks = {
+  koe: "./youbou.html",
+  request: "./youbou.html",
+  disappointing: "./disappointing.html"
+};
+
+// 本番
+// const address = "tos_oka2@palcoop.or.jp";
+
+// テスト
+const address = "kumikoetest@palcoop.or.jp";
