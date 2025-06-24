8c64137429e03ca5f5a66f52f4fb8103018923db
KASI-pro <78564295+KASI-pro@users.noreply.github.com>
2025-06-24 11:52:13 +0900

    Update links.js

--- links.js
+++ links.js
@@ -1,19 +1,17 @@
-/*
-// 本番
-const address = 'tos_oka2@palcoop.or.jp';
-const voiceLinks = {
-  koe: "./kumikoe.html",
-  request: "./youbou.html",
-  disappointing: "./disappointing.html"
-};
-*/
+const isTest = false; // ← ここが true だとテスト、false だと本番！
 
+const address = isTest
+  ? 'kumikoetest@palcoop.or.jp'
+  : 'tos_oka2@palcoop.or.jp';
 
-// テスト
-const address = 'kumikoetest@palcoop.or.jp';
-
-const voiceLinks = {
-  koe: "./kumikoeTest.html",
-  request: "./youbouTest.html",
-  disappointing: "./disappointingTest.html"
-};
+const voiceLinks = isTest
+  ? {
+      koe: "./kumikoeTest.html",
+      request: "./youbouTest.html",
+      disappointing: "./disappointingTest.html"
+    }
+  : {
+      koe: "./kumikoe.html",
+      request: "./youbou.html",
+      disappointing: "./disappointing.html"
+    };
